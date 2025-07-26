90ddea1cfe13986c0121170c7187d9e21ab60bd6
Osank31 <osankverma2004@gmail.com>
2025-07-26 18:26:29 +0530

    just the backend

---
+++ src/app/api/generate-session/route.ts
@@ -0,0 +1,29 @@
+import { v4 as uuidv4 } from 'uuid';
+import { NextRequest, NextResponse } from 'next/server';
+import { sessionChatTable } from '@/config/schema';
+import db from '@/config/db';
+import { currentUser } from '@clerk/nextjs/server';
+
+export async function POST(req: NextRequest) {
+    const body = await req.json()
+    const {note}: {note: string} = body || ''
+    const sessionId = uuidv4();
+    const user = await currentUser()
+    
+    
+    try {
+        if(!user){
+            return NextResponse.json({message: 'user not found'})
+        }
+        const result = await db.insert(sessionChatTable).values({
+            sessionId,
+            notes: note,
+            conversation: null,
+            report: null,
+            createdBy: user.id
+        }).returning()
+        return NextResponse.json(result[0]);
+    } catch (error) {
+        return NextResponse.json(error);
+    }
+}
--- src/config/schema.ts
+++ src/config/schema.ts
@@ -1,8 +1,17 @@
-import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
+import { integer, pgTable, varchar, text, json } from "drizzle-orm/pg-core";
 
 export const usersTable = pgTable("users", {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
   name: varchar({ length: 255 }).notNull(),
   email: varchar({ length: 255 }).notNull().unique(),
   credits: integer()
+});
+
+export const sessionChatTable = pgTable("session_chat", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  sessionId: varchar({ length: 255 }).notNull(),
+  notes: text(),
+  conversation: json().default(null),
+  report: json().default(null),
+  createdBy: varchar({ length: 255 }).notNull()
 });
